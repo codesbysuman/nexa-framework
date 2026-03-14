90894943a0fba6fea508065cab658b122d9c2129
Suman Sutradhar <codebysuman@gmail.com>
2026-03-14 22:39:31 +0530

    removed unnecessory export

--- index.js
+++ index.js
@@ -2,4 +2,3 @@ export * from "./src/nexa-fetcher/index.js";
 export * from "./src/nexa-renderer/index.js";
 export * from "./src/nexa-router/index.js";
 export * from "./src/nexa-state/index.js";
-export * from "./src/nexa-static-helpers/index.js";
